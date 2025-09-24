5addffff6eba49f6f38ab6ce7981db6dcf4c62c2
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-09-24 21:06:56 +0530

    added authRouter

---
+++ server/src/routers/authRouter.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const { validateSignupData } = require("../utils/validate");
+const { userModel } = require("../models/user");
+
+const authRouter = express.Router();
+
+authRouter.post("/auth/signup", async (req, res, next) => {
+  try {
+    const { name, email, password, bio } = req.body;
+
+    validateSignupData(req);
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new userModel({
+      name: name,
+      email: email,
+      password: hashedPassword,
+      bio: bio,
+    });
+
+    const newUser = await user.save();
+    res.json({ message: `User added successfully`, data: newUser });
+  } catch (err) {
+    next(err);
+  }
+});
+
+authRouter.post("/auth/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const foundUserData = await userModel.findOne({ email: email });
+    if (!foundUserData) {
+      throw new Error("Invalid Credential!");
+    }
+
+    const isPasswordSame = await bcrypt.compare(
+      password,
+      foundUserData.password
+    );
+
+    if (isPasswordSame) {
+      const token = jwt.sign(
+        { _id: foundUserData._id },
+        "#MyBlogSecreKey1234----",
+        {
+          expiresIn: "1d",
+        }
+      );
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
+
+      res.json({
+        message: "Login Successful",
+      });
+    } else {
+      throw new Error("Invalid Credential");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = authRouter;
