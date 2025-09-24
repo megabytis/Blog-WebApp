8011f437540cca5de8c1a50977a6e0889ac769fe
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-09-24 23:25:15 +0530

    created Auth middleware

---
+++ server/src/middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const { userModel } = require("../models/user");
+
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+
+    if (!token) {
+      throw new Error("Token not valid!");
+    }
+
+    const foundUserObject = jwt.verify(token, "#MyBlogSecreKey1234----");
+
+    const { _id } = foundUserObject;
+
+    const foundUser = userModel.findById(_id);
+
+    if (!foundUser) {
+      throw new Error("User not found!");
+    }
+
+    req.user = foundUser;
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { userAuth };
