77caa4530704b30abfc0a663ba9f574ec291fcbd
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-09-25 11:16:32 +0530

    created post schema

---
+++ server/src/models/posts.js
@@ -0,0 +1,42 @@
+const { default: mongoose } = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    tags: [
+      {
+        type: String,
+        lowercase: true,
+        trim: true,
+      },
+    ],
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const postModel = mongoose.model("post", postSchema);
+
+module.exports = {
+  postModel,
+};
