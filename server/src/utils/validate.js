f107007945e2c5debf0fdd57570325d42d35df6a
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-09-25 11:17:05 +0530

    validating post data

--- server/src/utils/validate.js
+++ server/src/utils/validate.js
@@ -5,12 +5,24 @@ const validateSignupData = (req) => {
 
   if (!name) {
     throw new Error("Name not valid!");
-  }
-  if (!validator.isEmail(email)) {
+  } else if (!validator.isEmail(email)) {
     throw new Error("Email not valid!");
-  }
-  if (!validator.isStrongPassword(password)) {
+  } else if (!validator.isStrongPassword(password)) {
     throw new Error("Password is not Strong!");
+  }
+};
+
+const validatePostData = (req) => {
+  const { title, content, image, tags } = req.body;
+
+  if (!title) {
+    throw new Error("Title now valid!");
+  } else if (!content) {
+    throw new Error("Content not valid!");
+  } else if (!validator.isURL(image)) {
+    throw new Error("Not a Valid image URL!");
+  } else if (validator.isEmpty(tags)) {
+    throw new Error("Tags shouldn't be empty!");
   }
 };
 
