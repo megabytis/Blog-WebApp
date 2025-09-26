b4e57937ba23294368af8a46fd623a4de22a91e6
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-09-26 12:40:52 +0530

    created /post/update API

--- server/src/routers/postRouter.js
+++ server/src/routers/postRouter.js
@@ -2,12 +2,18 @@ const express = require("express");
 
 const { userAuth } = require("../middleware/auth");
 const { postModel } = require("../models/posts");
+const {
+  validatePostData,
+  validateUpdatePostData,
+} = require("../utils/validate");
 
 const postRouter = express.Router();
 
 postRouter.post("/post/create", userAuth, async (req, res, next) => {
   try {
     const { title, content, image, tags } = req.body;
+
+    validatePostData(req);
 
     const Post = new postModel({
       title: title,
@@ -29,6 +35,42 @@ postRouter.post("/post/create", userAuth, async (req, res, next) => {
     const savedPost = await Post.save();
 
     res.json({ message: "Post saved Successfully!", data: savedPost });
+  } catch (err) {
+    next(err);
+  }
+});
+
+postRouter.patch("/post/update/:postID", userAuth, async (req, res, next) => {
+  try {
+    const loggedInUser = req.user;
+    const postID = req.params?.postID;
+    const dataUserWannaModify = req.body;
+    const ALLOWED_FIELDS_TO_UPDATE = ["title", "content", "tags", "image"];
+
+    validateUpdatePostData(req);
+
+    const foundPost = await postModel.findById(postID);
+
+    if (foundPost === null) {
+      throw new Error("Post not Found!");
+    }
+
+    if (foundPost.author.toString() !== loggedInUser._id.toString()) {
+      throw new Error("You are not Authorized to Update the Post!");
+    }
+
+    ALLOWED_FIELDS_TO_UPDATE.forEach((field) => {
+      if (field in req.body) {
+        foundPost[field] = req.body[field];
+      }
+    });
+
+    const updatedPost = await foundPost.save();
+
+    res.json({
+      message: `Dear ${loggedInUser.name}, your Post has been updated Successfully!`,
+      data: foundPost,
+    });
   } catch (err) {
     next(err);
   }
