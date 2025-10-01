5e72a2cc826771b24c215804015808e55149e1ae
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-10-01 14:28:21 +0530

    fix

--- server/src/app.js
+++ server/src/app.js
@@ -37,6 +37,21 @@ app.use(
   })
 );
 
+// Handle preflight requests
+app.options("*", (req, res) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://blog-web-app-eight-olive.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.status(200).end();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
